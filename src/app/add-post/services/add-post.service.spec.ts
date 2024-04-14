304dff1d44dee428060a9f9f17851f1302027444
Sławomir Perlak <perlakslawomir@gmail.com>
2024-04-14 19:03:05 +0200

    Jest setup and first test

--- src/app/add-post/services/add-post.service.spec.ts
+++ src/app/add-post/services/add-post.service.spec.ts
@@ -1,16 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing'
+import {Firestore, collection, addDoc} from '@angular/fire/firestore'
+import {AddPostService} from './add-post.service'
+import {Observable, of} from 'rxjs'
+import {AddPostRequest} from '../types/addPostRequest'
+import * as AngularFireModule from '@angular/fire/firestore'
 
-import { AddPostService } from './add-post.service';
+jest.mock('@angular/fire/firestore')
+
+// Mock Firestore
+const firestoreMock = {
+  collection: jest.fn(),
+}
+
+// Mock Firestore collection reference
+const collectionMock = {
+  addDoc: jest.fn(),
+}
+
+// Sample post data for testing
+const samplePost: AddPostRequest = {
+  title: 'Test Post',
+  content: 'This is a test post.',
+  createdAt: new Date(),
+  userId: 'aaaaajjsektj',
+  userEmail: 'userEmail@email.com',
+}
 
 describe('AddPostService', () => {
-  let service: AddPostService;
+  let addPostService: AddPostService
+  let firestore: Firestore
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AddPostService);
-  });
+    TestBed.configureTestingModule({
+      providers: [
+        AddPostService,
+        {provide: Firestore, useValue: firestoreMock},
+      ],
+    })
+    addPostService = TestBed.inject(AddPostService)
+    firestore = TestBed.inject(Firestore)
+
+    // Mock Firestore methods
+    ;(firestoreMock.collection as jest.Mock).mockReturnValue(collectionMock)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(addPostService).toBeTruthy()
+  })
+})
